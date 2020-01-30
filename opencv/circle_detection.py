bcc75ff83dd74a1f48bae07b578f17e75b084561
Audi Liu <audi@Audis-MacBook-Pro.local>
2020-01-29 17:25:40 -0800

    laser commit 2: statistics

--- opencv/circle_detection.py
+++ opencv/circle_detection.py
@@ -10,37 +10,128 @@ import numpy as np
 import cv2
 import sys
 import time
+import signal
+
+
 #img = cv.imread('BLAH.png') #
 
 '''
 time per image
 when it's not a circle
-optimize the parameters to 
+optimize the parameters.
+
+WORK ON:
+ISSUE 1:
+performance varies greatly for different videos, especially with the sofa shit.
+1: good
+2 : b
+3 : b
+4 : good
+5 : good
+
+for vid2, check the canny edge image. decide to tune canny edge or param2
+
+
+ISSUE 2: false positives: NEED better white red detection.
+ALREADY SO LENIENT. BUT DAMN
+70 percent on real laser
+90 percent on false laser
+fuck.
+
+
+
 
 '''
+
+
 '''
 parameters for cv2.HoughCircles
 input:
-image	8-bit, single-channel, grayscale input image.
-method	Detection method, see HoughModes. Currently, the only implemented method is HOUGH_GRADIENT
-dp	Inverse ratio of the accumulator resolution to the image resolution. For example, if dp=1 , the accumulator has the same resolution as the input image. If dp=2 , the accumulator has half as big width and height.
-minDist	Minimum distance between the centers of the detected circles. If the parameter is too small, multiple neighbor circles may be falsely detected in addition to a true one. If it is too large, some circles may be missed.
-param1	First method-specific parameter. In case of HOUGH_GRADIENT , it is the higher threshold of the two passed to the Canny edge detector (the lower one is twice smaller).
-param2	Second method-specific parameter. In case of HOUGH_GRADIENT , it is the accumulator threshold for the circle centers at the detection stage. The smaller it is, the more false circles may be detected. Circles, corresponding to the larger accumulator values, will be returned first.
-minRadius	Minimum circle radius.
-maxRadius	Maximum circle radius. If <= 0, uses the maximum image dimension. If < 0, returns centers without finding the radius.
+image    8-bit, single-channel, grayscale input image.
+method    Detection method, see HoughModes. Currently, the only implemented method is HOUGH_GRADIENT
+dp    Inverse ratio of the accumulator resolution to the image resolution. For example, if dp=1 , the accumulator has the same resolution as the input image. If dp=2 , the accumulator has half as big width and height.
+minDist    Minimum distance between the centers of the detected circles. If the parameter is too small, multiple neighbor circles may be falsely detected in addition to a true one. If it is too large, some circles may be missed.
+
+param1    First method-specific parameter. In case of HOUGH_GRADIENT , it is the higher threshold of the two passed to the Canny edge detector (the lower one is twice smaller).
+param2    Second method-specific parameter. In case of HOUGH_GRADIENT , it is the accumulator threshold for the circle centers at the detection stage. The smaller it is, the more false circles may be detected. Circles, corresponding to the larger accumulator values, will be returned first.
+minRadius    Minimum circle radius.
+maxRadius    Maximum circle radius. If <= 0, uses the maximum image dimension. If < 0, returns centers without finding the radius.
 
 output:
-circles	Output vector of found circles. Each vector is encoded as 3 or 4 element floating-point vector (x,y,radius) or (x,y,radius,votes) .
+circles    Output vector of found circles. Each vector is encoded as 3 or 4 element floating-point vector (x,y,radius) or (x,y,radius,votes) .
 
 '''
 
 
 '''
-param1: 30 and 150 have similar results. hm
-param2: higher, less circles:
+dp: anything larger than 2 yields too many cirlces. 0.5 gives about the same result. so far do 1
+minDist:
+    
+param1: lower, less edges get filtered out.
+param2: higher, less circles: 40 is optimal now
+
+
+# circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 50,
+    param1=1, param2=33, minRadius=10, maxRadius=40) #max 60 pixel cir
+
 '''
 #    cv2.Smooth(gray, gray, cv2.CV_GAUSSIAN, 7, 7)
+total_time = 0
+total_circles = 0
+no_circle = 0
+no_laser = 0
+frames = 0
+
+
+white_detected = 0
+red_detected = 0
+
+avg_circles = 0
+circle_detection = 0 #percent of time actually detected a circle
+laser_detection = 0 #percentage of time detected a laser  given a circle
+avg_time = 0
+
+
+
+def detect_circle(frame): # Display the resulting frame, WITH THE LASER CIRCLED
+    global total_circles
+    global no_circle
+    edges = canny_edge(frame, 30) #if this num is too low, less circles detected and high chance of no circled deteced
+
+    gray = cv2.cvtColor(edges, cv2.COLOR_BGR2GRAY)
+    #gray = cv2.medianBlur(gray, 5)
+    
+    #print("detecting circles...")
+    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 50,
+    param1=1, param2=33, minRadius=10, maxRadius=40) #max 60 pixel circle #50 , param2 small = more false circles
+    
+    #print("cirlces:")
+    #print(circles)
+    
+    #circles = np.uint16(np.around(circles))
+    count = 0
+    if circles is not None:
+         ignore, count, ignore2 = circles.shape
+         #if count > 10: if too many adjust param1 for cann_edge and param2
+                
+         print("detected %s circle(s)" %count)
+         total_circles += count
+
+         
+          #save the detected circles iamges for anaylysis:
+         #im.save("./laser_circles" + str())
+         
+    else:
+        print("Failed to detect laser in this frame, skip and continue")
+        no_circle +=1
+        return np.asarray([])
+   
+    #print(circles)
+    #print(circles.shape) #1 by how many circles by 3 (x, y, and radius)
+    
+    return circles
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
 
 
 #draw the circles onto the frame
@@ -48,7 +139,7 @@ param2: higher, less circles:
 
 def draw_circles(frame, circles):
     #print(circles.shape)
-    print(circles)
+    #print(circles)
     
     #shape = circles.shape
     #print(type(shape))
@@ -70,6 +161,11 @@ def draw_circles(frame, circles):
 
 
 def filter_red(frame, circles): #1920*1080*3
+    global no_laser
+    global red_detected
+    global white_detected
+    
+    red_detected
     if circles.ndim == 1:#one circle
         return circles
 
@@ -77,19 +173,29 @@ def filter_red(frame, circles): #1920*1080*3
         for circle in circles[0, :]: #cirlce = (x, y ,radius)
             x = int(circle[0])
             y = int(circle[1])
-            r,g,b = frame[y,x] 
-            white = (r > 220 and g > 220 and b > 220)
-            red = (r > 120 and g > 40 and b > 60)
+            r,g,b = frame[y,x]
+            #print(frame[y,x] )
+            white = (r > 200 and g > 200 and b > 200) #(r > 220 and g > 220 and b > 220)
+                red = (r > 100 and g > 30 and b > 40) #(r > 120 and g > 40 and b > 60)
+            #if white or red:
+                #return circle
+            
+            if white:
+                white_detected+=1
+            if red:
+                red_detected+=1
+            
             if white or red:
                 return circle
             
         print("failed to find white or red center")
+        no_laser += 1
         return np.asarray([])
 
         '''
         #for debugging only
         coord = (x,y)
-        rgb = frame[y,x] 
+        rgb = frame[y,x]
         print("coord:" + str(coord) +  " rgb: " +  str(rgb) )
         '''
     else:
@@ -106,45 +212,37 @@ def canny_edge(frame, val): #output a grayscale image of edges, easier for cv2.H
     img_blur = cv2.blur(frame, (3,3))
     detected_edges = cv2.Canny(img_blur, low_threshold, low_threshold*ratio, kernel_size)
     mask = detected_edges != 0
-    dst = frame * (mask[:,:,None].astype(frame.dtype)) #numpy array 
+    dst = frame * (mask[:,:,None].astype(frame.dtype)) #numpy array
     #cv2.imshow('edges', dst)
     return dst
 
 
 
-def detect_circle(frame): # Display the resulting frame, WITH THE LASER CIRCLED
-    edges = canny_edge(frame, 30) #if this num is too low, less circles detected and high chance of no circled deteced
+def signal_handler(sig, frame):
+        print('You pressed Ctrl+C!')
+        avg_circles = total_circles/frames
+        circle_detection = 100 - no_circle/frames * 100
+        laser_detection = 100 - no_laser/(frames - no_circle) *100
+        red_detection = 100 * red_detected/(frames - no_circle)
+        white_detection = 100 * white_detected/(frames - no_circle)
 
-    gray = cv2.cvtColor(edges, cv2.COLOR_BGR2GRAY)
-    #gray = cv2.medianBlur(gray, 5)
-    
-    #print("detecting circles...")
-    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 30,
-    param1=30, param2=40, minRadius=0, maxRadius=50) #max 60 pixel circle #50 , param2 small = more false circles
-    
-    #print("cirlces:")
-    #print(circles)
-    
-    #circles = np.uint16(np.around(circles))
-    count = 0
-    if circles is not None:
-         ignore, count, ignore2 = circles.shape
-         print("detected %s circle(s)" %count)
-         
-          #save the detected circles iamges for anaylysis:
-         #im.save("./laser_circles" + str())
-         
-    else:
-        print("Failed to detect laser in this frame, skip and continue")
-        return np.asarray([])
-   
-    #print(circles)
-    #print(circles.shape) #1 by how many circles by 3 (x, y, and radius)
-    
-    return circles
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
+        avg_time = total_time/frames
+        
+        print("\nSUMMARY:")
+        print("frames:", frames)
+        print("%s average circles detected per frame" %round(avg_circles,2))
+        print("%s percent circle_detection" %round(circle_detection,2) )
+        print("%s percent laser_detection" %round(laser_detection,2) )
+        
+        print("%s percent red_detection" %round(red_detection,2) )
+        print("%s percent white_detection" %round(white_detection,2) )
 
+
+        print("%s second per frame" % avg_time )
+        sys.exit(0)
+        
+signal.signal(signal.SIGINT, signal_handler)
+#signal.pause()
 
 
 
@@ -153,54 +251,75 @@ def detect_circle(frame): # Display the resulting frame, WITH THE LASER CIRCLED
 cap = cv2.VideoCapture('/Users/audi/Desktop/x1/Link_Laser_Samples/laser_sample_1/video.mp4')
  
 # Check if camera opened successfully
-if (cap.isOpened()== False): 
+if (cap.isOpened()== False):
   print("Error opening video stream or file")
  
 # Read until video is completed
-totoal_circles = 0
-frames = 0
-avg_circles_= 0
-
-no_circle = 0
-
 
 while(cap.isOpened()):
   # Capture frame-by-frame
   ret, frame = cap.read()
-  if ret == True: 
+
+  if ret == True:
+    start_time = time.time()
+
     #original_frame = np.copy(frame)
     detected_circles = detect_circle(frame)
-    print(detected_circles)
+    #print(detected_circles)
+    #draw_circles(frame, detected_circles) #will modify frame to show the circles
     
-    draw_circles(frame, detected_circles) #will modify frame to show the circles
+    
     #time.sleep(2)
-    #red_circle = filter_red(frame, detected_circles)
+    red_circle = filter_red(frame, detected_circles)
     #print(red_circle)
-    #draw_circles(frame, red_circle) #will modify frame to show the circles
+    
+    time_elapsed =  time.time() - start_time
+    total_time += time_elapsed
+    
+    #NOTE: DON'T EVER DRAW BEFORE YOU DETECT, WILL MESS UP THE IMAGE DATA
+    draw_circles(frame, red_circle) #will modify frame to show the circles
     #time.sleep(2)
+    
     
     #DO SOME SUMMARY STATISTICS
-    count = len(detected_circles)
     
-    totoal_circles += count
-    if count == 0:
-        no_circle += 1
+    #if len(detected_circles) == 0:
+     #   no_circle += 1
+        
         
     frames += 1
-    avg_circles = totoal_circles/frames
-    no_circle_percent = no_circle/frames * 100
+    
+    
+    
     
     # Press Q on keyboard to  exit
     if cv2.waitKey(25) & 0xFF == ord('q'):
+        avg_circles = total_circles/frames
+        circle_detection = 100 - no_circle/frames * 100
+        laser_detection = 100 - no_laser/(frames - no_circle) *100
+        red_detection = 100*red_detected/(frames - no_circle)
+        white_detection = 100*white_detected/(frames - no_circle)
+
+        avg_time = total_time/frames
+        
         print("\nSUMMARY:")
         print("frames:", frames)
         print("%s average circles detected per frame" %round(avg_circles,2))
-        print("%s percent of the time: Failed to identify any cirlce" %round(no_circle_percent,2) )
+        print("%s percent circle_detection" %round(circle_detection,2) )
+        print("%s percent laser_detection" %round(laser_detection,2) )
+        
+        print("%s percent red_detection" %round(red_detection,2) )
+        print("%s percent white_detection" %round(white_detection,2) )
+
+
+        print("%s second per frame" % avg_time )
         break
   # Break the loop
   else:
     print("Failed to load frame:")
     break
+
+
 
  
 # When everything done, release the video capture object
