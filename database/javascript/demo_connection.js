80bf226f77482e9929eed283249d7d291ff9526f
anthonytate <anthonydt3817@gmail.com>
2020-05-06 16:46:33 -0700

    finished javascript and python files

--- database/javascript/demo_connection.js
+++ database/javascript/demo_connection.js
@@ -1,57 +1,95 @@
 var mysql = require('mysql');
 var fs = require('fs');
 
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "tate",
-    password: "pass123",
-    database: "x1_guardian"
-});
-
-function lastNVals(count) {
-    var results = [];
-    con.connect(function(err) {
-        if (err) throw err;
-        var sel = "SELECT * FROM position_data ORDER BY id DESC LIMIT ";
-        sel = sel.concat(count.toString());
-        con.query(sel, function(err, result, fields) {
-            if (err) throw err;
-            result = JSON.stringify(result);
-            results.push(result);
-            console.log(result);
+lastNVals = function(count){
+    return new Promise(function(resolve, reject){
+        var con = mysql.createConnection({
+            host: "localhost",
+            user: "tate",
+            password: "pass123",
+            database: "x1_guardian"
         });
-    });
-    return results;
+        con.query("SELECT * FROM position_data ORDER BY id DESC LIMIT ?", [count], function(err, results) {
+            con.end();
+            if(results === undefined){
+                 reject(new Error("Error rows is undefined"));             
+            } else {  
+                var parsed_results = [];  
+                if(results.length){
+                    var line = JSON.stringify(results);
+                    lines = line.split("},");
+                    for (var i = 0; i < lines.length; i++) {
+                        lines[i] = lines[i].replace( /[{}\[\]"]/g, "");
+                        var split_string = lines[i].split(",");
+                        var final_string = "";
+                        for (var j = 0; j < split_string.length; j++) {
+                            split_string[j] = split_string[j].replace( /^.+["a-zA-Z]:/, "");
+                            final_string = final_string.concat(split_string[j], ",");
+                        }
+                        final_string = final_string.slice(0, -1);
+                        parsed_results.push(final_string);
+                    }
+                 }             
+                resolve(parsed_results);             
+            }
+        }
+    )}
+)}
+
+lastNVals(5) 
+.then(function(results){   
+    render(results) 
+}) 
+.catch(function(err){   
+    console.log("Promise rejection error: "+err); 
+})  
+render = function(results){ 
+    console.log(results);  
 }
 
-function getAllVals() {
-    con.connect(function(err) {
-        if (err) throw err;
-        con.query("SELECT * FROM position_data", function(err, result, fileds) {
-            if (err) throw err;
-            fs.open("log.csv", "w", function (err, file) {
-                if (err) throw err;
-            });
-            fs.appendFile("log.csv", "ID,Link_x,Link_y,Laser_x,Laser_y,Time\n", function (err) {
-                if (err) throw err;
-            });
-            for (var i = 0; i < result.length; i++) {
-                result[i] = JSON.stringify(result[i]);
-                result[i] = result[i].replace(/[{}]/g, "");
-                var split_result = result[i].split(",");
-                var final_string = "";
-                for (var j = 0; j < split_result.length; j++) {
-                    split_result[j] = split_result[j].replace( /^.+["a-zA-Z]:/, "");
-                    split_result[j] = split_result[j].replace( /"/g, "");
-                    final_string = final_string.concat(split_result[j], ",");
-                }
-                final_string = final_string.slice(0, -1);
-                final_string = final_string.concat("\n");
-                console.log(final_string);
-                fs.appendFile('log.csv', final_string, function (err) {
-                    if (err) throw err;
-                });
-            }
+getAllVals = function(){
+    return new Promise(function(resolve, reject){
+        var con = mysql.createConnection({
+            host: "localhost",
+            user: "tate",
+            password: "pass123",
+            database: "x1_guardian"
         });
-    });
+        con.query("SELECT * FROM position_data", function(err, results) {
+            con.end();
+            if(results === undefined){
+                 reject(new Error("Error rows is undefined"));             
+            } else {  
+                var returned_string = "";  
+                if(results.length){
+                    var line = JSON.stringify(results);
+                    lines = line.split("},");
+                    for (var i = 0; i < lines.length; i++) {
+                        lines[i] = lines[i].replace( /[{}\[\]"]/g, "");
+                        var split_string = lines[i].split(",");
+                        var final_string = "";
+                        for (var j = 0; j < split_string.length; j++) {
+                            split_string[j] = split_string[j].replace( /^.+["a-zA-Z]:/, "");
+                            final_string = final_string.concat(split_string[j], ",");
+                        }
+                        final_string = final_string.slice(0, -1);
+                        returned_string = returned_string.concat(final_string, '\n');
+                        final_string = final_string.slice(0, -1);
+                    }
+                 }             
+                resolve(returned_string);             
+            }
+        }
+    )}
+)}
+
+getAllVals() 
+.then(function(results){   
+    render(results) 
+}) 
+.catch(function(err){   
+    console.log("Promise rejection error: "+err); 
+})  
+render = function(results){ 
+    console.log(results);  
 }
